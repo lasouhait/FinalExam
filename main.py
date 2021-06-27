ed12ef1f0d10c139ff3d47acf5be2294deef3b12
lasouhait <62388539+lasouhait@users.noreply.github.com>
2021-06-27 21:02:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,9 @@ Q2 = st.radio("2.",('A','B','C','D'))
 st.image("./"+path+"/3.jpg")
 Q3 = st.radio("3.",('A','B','C','D'))
 
+st.image("./"+path+"/4.jpg")
+Q4 = st.radio("4.",('A','B','C','D'))
+
 st.image("./"+path+"/5.jpg")
 Q5 = st.radio("5.",('A','B','C','D'))
 
@@ -30,11 +33,15 @@ st.image("./"+path+"/67.jpg")
 Q6 = st.radio("6.",('A','B','C','D'))
 Q7 = st.radio("7.",('A','B','C','D'))
 
+st.image("./"+path+"/8.jpg")
+Q8 = st.radio("8.",('A','B','C','D'))
+
 st.image("./"+path+"/9.jpg")
-Q9 = st.radio("1.",('A','B','C','D'))
+Q9 = st.radio("9.",('A','B','C','D'))
 
 submit = pd.DataFrame(['題號','答案'])
 submit = submit.append(1,Q1)
 submit = submit.append(2,Q2)
 
 st.table(submit)
+st.to_csv("./"+path+"0901.csv",encoding="utf-8")
