a48aba55891768494cf0196f7a8ef33747aba351
lasouhait <62388539+lasouhait@users.noreply.github.com>
2021-06-27 10:38:54 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+import streamlit as st
+import pandas as pd
+import statsmodels.api as sm
+
+st.set_page_config(page_title="期末考頁面")
+st.title("109年度第二學期期末考")
+#st.write("All information is for personal use, and is not aimed to provide suggestion.")
+#page_nav = st.sidebar.radio("請選取頁面",["營運概況","人力資訊","董監酬勞揭露","股價資訊","金融統計"])
