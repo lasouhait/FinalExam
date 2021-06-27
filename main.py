c99ea20c06dcd499a664f4b2a5d4c0f50b7bdb60
lasouhait <62388539+lasouhait@users.noreply.github.com>
2021-06-27 22:30:58 +0800

    Update main.py

--- main.py
+++ main.py
@@ -12,55 +12,58 @@ st.title("109年度第二學期期末考")
 path="國二下"
 answer = pd.read_csv("./"+path+"/正解.csv")
 
-st.write(answer)
+#st.write(answer)
 
-st.image("./"+path+"/1.jpg")
-Q1 = st.radio("1.",('A','B','C','D'))
+if path=="國二下":
+  st.image("./"+path+"/1.jpg")
+  Q1 = st.radio("1.",('A','B','C','D'))
 
-st.image("./"+path+"/2.jpg")
-Q2 = st.radio("2.",('A','B','C','D'))
+  st.image("./"+path+"/2.jpg")
+  Q2 = st.radio("2.",('A','B','C','D'))
 
-st.image("./"+path+"/3.jpg")
-Q3 = st.radio("3.",('A','B','C','D'))
+  st.image("./"+path+"/3.jpg")
+  Q3 = st.radio("3.",('A','B','C','D'))
 
-st.image("./"+path+"/4.jpg")
-Q4 = st.radio("4.",('A','B','C','D'))
+  st.image("./"+path+"/4.jpg")
+  Q4 = st.radio("4.",('A','B','C','D'))
 
-st.image("./"+path+"/5.jpg")
-Q5 = st.radio("5.",('A','B','C','D'))
+  st.image("./"+path+"/5.jpg")
+  Q5 = st.radio("5.",('A','B','C','D'))
 
-st.image("./"+path+"/67.jpg")
-Q6 = st.radio("6.",('A','B','C','D'))
-Q7 = st.radio("7.",('A','B','C','D'))
+  st.image("./"+path+"/67.jpg")
+  Q6 = st.radio("6.",('A','B','C','D'))
+  Q7 = st.radio("7.",('A','B','C','D'))
 
-st.image("./"+path+"/8.jpg")
-Q8 = st.radio("8.",('A','B','C','D'))
+  st.image("./"+path+"/8.jpg")
+  Q8 = st.radio("8.",('A','B','C','D'))
 
-st.image("./"+path+"/9.jpg")
-Q9 = st.radio("9.",('A','B','C','D'))
+  st.image("./"+path+"/9.jpg")
+  Q9 = st.radio("9.",('A','B','C','D'))
 
-st.image("./"+path+"/1011.jpg")
-Q10 = st.radio("10.",('A','B','C','D'))
-Q11 = st.radio("11.",('A','B','C','D'))
+  st.image("./"+path+"/1011.jpg")
+  Q10 = st.radio("10.",('A','B','C','D'))
+  Q11 = st.radio("11.",('A','B','C','D'))
 
-st.image("./"+path+"/1213.jpg")
-Q12 = st.radio("12.",('A','B','C','D'))
-Q13 = st.radio("13.",('A','B','C','D'))
+  st.image("./"+path+"/1213.jpg")
+  Q12 = st.radio("12.",('A','B','C','D'))
+  Q13 = st.radio("13.",('A','B','C','D'))
 
-st.image("./"+path+"/14.jpg")
-Q14 = st.radio("14.",('A','B','C','D'))
+  st.image("./"+path+"/14.jpg")
+  Q14 = st.radio("14.",('A','B','C','D'))
 
-st.image("./"+path+"/15.jpg")
-Q15 = st.radio("15.",('A','B','C','D'))
+  st.image("./"+path+"/15.jpg")
+  Q15 = st.radio("15.",('A','B','C','D'))
 
-st.image("./"+path+"/1617.jpg")
-Q16 = st.radio("16.",('A','B','C','D'))
-Q17 = st.radio("17.",('A','B','C','D'))
+  st.image("./"+path+"/1617.jpg")
+  Q16 = st.radio("16.",('A','B','C','D'))
+  Q17 = st.radio("17.",('A','B','C','D'))
 
-st.image("./"+path+"/1819.jpg")
-Q18 = st.radio("18.",('A','B','C','D'))
-Q19 = st.radio("19.",('A','B','C','D'))
+  st.image("./"+path+"/1819.jpg")
+  Q18 = st.radio("18.",('A','B','C','D'))
+  Q19 = st.radio("19.",('A','B','C','D'))
 
+  st.image("./"+path+"/20.jpg")
+  Q20 = st.radio("20.",('A','B','C','D'))
 
 submit = pd.DataFrame(columns=['題號','答案'])
 submit = submit.append({'題號':1,'答案':Q1},ignore_index=True)
@@ -82,7 +85,9 @@ submit = submit.append({'題號':16,'答案':Q16},ignore_index=True)
 submit = submit.append({'題號':17,'答案':Q17},ignore_index=True)
 submit = submit.append({'題號':18,'答案':Q18},ignore_index=True)
 submit = submit.append({'題號':19,'答案':Q19},ignore_index=True)
+submit = submit.append({'題號':20,'答案':Q20},ignore_index=True)
 
-st.write(submit)
+  if st.button('檢查提交答案'):
+    st.write(submit)
 st.markdown('<a href="mailto:daniel40307@yahoo.com.tw">Contact us !</a>', unsafe_allow_html=True)
 #submit.to_csv("./"+path+"0901.csv",encoding="utf-8")
